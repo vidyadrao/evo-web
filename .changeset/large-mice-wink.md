f868b68afb2cb2e72724020dc1a1024ddf35b211
Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>
2025-09-02 16:01:02 +0200

    feat(section-notice): update cta without footer to latest specs (#257)

---
+++ .changeset/large-mice-wink.md
@@ -0,0 +1,5 @@
+---
+"@ebay/skin": patch
+---
+
+feat(section-notice): update cta without footer to latest specs
--- packages/skin/src/sass/section-notice/stories/icon-16.stories.js
+++ packages/skin/src/sass/section-notice/stories/icon-16.stories.js
@@ -130,6 +130,21 @@ export const dismissAndFakeLinkCTA = () => `
 </div>
 `;
 
+export const noDismissAndLinkCTA = () => `
+<div class="section-notice section-notice--confirmation" role="region">
+    <div class="section-notice__header" role="region" aria-roledescription="Notice">
+        <svg aria-hidden="true" class="icon icon--16">
+            <use href="#icon-confirmation-filled-16"></use>
+        </svg>
+    </div>
+    <div class="section-notice__main">
+        <h3 class="section-notice__title">Notification Title</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+    <p class="section-notice__cta"><a href="https://www.ebay.com">Action</a></p>
+</div>
+`;
+
 // known issue: https://github.com/eBay/skin/issues/2142
 export const RTL = () => `
 <div dir="rtl">
@@ -151,6 +166,23 @@ export const RTL = () => `
                 </svg>
             </button>
         </div>
+    </div>
+</div>
+`;
+
+export const noDismissAndLinkCTA_RTL = () => `
+<div dir="rtl">
+    <div class="section-notice section-notice--confirmation" role="region">
+        <div class="section-notice__header" role="region" aria-roledescription="Notice">
+            <svg aria-hidden="true" class="icon icon--16">
+                <use href="#icon-confirmation-filled-16"></use>
+            </svg>
+        </div>
+        <div class="section-notice__main">
+            <h3 class="section-notice__title">Notification Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <p class="section-notice__cta"><a href="https://www.ebay.com">Action</a></p>
     </div>
 </div>
 `;
